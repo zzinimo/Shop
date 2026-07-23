c381be550f64227f18a68b0109f047b4d39594aa
zzinimo <zachzinimon@gmail.com>
2026-07-23 10:35:28 -0400

    Resolve merge conflict markers in orders router

--- Server_Side/routes/ordersRouter.js
+++ Server_Side/routes/ordersRouter.js
@@ -13,10 +13,8 @@ router.get("/:id", getOrderById);
 router.post("/", createOrder);
 router.patch("/:id/status", updateStatus);
 router.patch("/:id/cancel", cancelOrder);
-<<<<<<< Updated upstream
+
 router.delete("/:id/delete", deleteOrder);
-=======
 router.delete("/:id", deleteOrder);
->>>>>>> Stashed changes
 
 module.exports = router;
